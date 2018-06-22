51c9a7e22d2d0d28e60bfcce408b9132075dec0f
sandeshgiri <sandesh.giri@gmail.com>
2018-06-22 12:46:27 +0530

    pqr

---
+++ src/test/java/Test/ATest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ATest {
+	
+	@Test
+	public void one()
+	{
+		Assert.assertTrue(true);
+		
+	}
+	
+	
+	@Test
+	public void two()
+	{
+		Assert.assertTrue(true);
+	}
+
+}
